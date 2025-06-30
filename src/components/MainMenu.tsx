1ff0585c15c2260bbde06cd322daa11922890298
Mulc1ber <mike@m.ru>
2025-06-30 21:23:41 +0300

    chore: translating navigation to NavLink

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
 
 export const MainMenu = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">
+        <Link to="/" className="navbar-brand">
           <h1>Книга контактов</h1>
-        </Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="/groups">Группы</Nav.Link>
-          <Nav.Link href="/favorit">Избранное</Nav.Link>
+        </Link>
+        <Nav className="me-auto gap-2">
+          <NavLink
+            to="/groups"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Группы
+          </NavLink>
+          <NavLink
+            to="/favorit"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Избранное
+          </NavLink>
         </Nav>
       </Container>
     </Navbar>
